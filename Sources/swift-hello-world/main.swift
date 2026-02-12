64ed7c3d6a94dc39112be376cb61f17f482aa243
David Gamero <david340804@gmail.com>
2026-02-12 15:04:17 -0500

    Bind Vapor to all interfaces

--- Sources/swift-hello-world/main.swift
+++ Sources/swift-hello-world/main.swift
@@ -1,6 +1,7 @@
 import Vapor
 
-let app = Application()
+var env = Environment(name: "development", arguments: ["vapor", "serve", "--hostname", "0.0.0.0", "--port", "8080"])
+let app = Application(env)
 defer { app.shutdown() }
 
 app.get { _ in
